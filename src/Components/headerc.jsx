135fd46b1ffb8e4e344701ab05b3ae083f6fe7bf
EBAKO CORNELIUS <ebakocornelius@gmail.com>
2025-03-21 13:08:14 +0200

    adding and working on chages

--- src/Components/headerc.jsx
+++ src/Components/headerc.jsx
@@ -29,7 +29,7 @@ function Headerc() {
           <div className="part2-1">
             <div className="call">
               <img
-                src="https://s3-alpha-sig.https://s3-alpha-sig.figma.com/img/7b44/3779/e00233c67a961c8bfdf2e37a4a1ceb1b?Expires=1743379200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=OIXTEHBoMp0v3sYrmft7a8HbfWMkS30hEUidZC3i8tCNFy59BFt45JM7aR7RXyi9zOh0jymydP9AsY9U0mXdmpEk3NNyON859VXBNRlUVWGFJfLPht0g72WnoKtbsqaMfyp00we7Wpcjcv3n-fD6Z2f8uO3d5enVq6bSxuSWm46HMkFHvnAK0hU0oEITKlvzQHADUzEAM-36DkDkUjTQzzSJ9a3glGTtZnYffi~AXtI8ZgJqSDnmwiWIpWBdyykn4B4W8Ll3x4Qor6FYHXBrTYYdyVIWJ4lnOh3kVK9dGSmmULpfxWsEqbEo4goJDSKbyIQshdODMR2mwKnVqgxjiQ__.com/img/7b44/3779/e00233c67a961c8bfdf2e37a4a1ceb1b?Expires=1741564800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=nZvHZDGhrEn8fO~B-UrrPF3rR3YC1LDOl5yjBGHY-XC8dvcr7KvR3g6OFULOUHYwPJxi-oq-YSL3SolVgZV9j0Qbz3EjxhIAhBB~jTbQArb9N9LHpJvSYMbJBDZ3oicFjWf8zdoXD~ZeXTf3yJUhfvyCnpwz~hzrgpCRoMAYQbdrqdB2VNvKh8cJehsN-XBtTslfx7HjhBbkJwll~Js7scLiPzPmN-rp8PqIRB~fjCQL0xgqocI0~LnVpYAwXhLjBUP~ketSartDVM2t4esVa8l8XBPNLkdLheHGb2XbyfxcSY692DHN8C6rVoPi7Ko7Eh3ZggbbU5AwES1n10Z50Q__"
+                src="https://s3-alpha-sig.figma.com/img/7b44/3779/e00233c67a961c8bfdf2e37a4a1ceb1b?Expires=1741564800&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=nZvHZDGhrEn8fO~B-UrrPF3rR3YC1LDOl5yjBGHY-XC8dvcr7KvR3g6OFULOUHYwPJxi-oq-YSL3SolVgZV9j0Qbz3EjxhIAhBB~jTbQArb9N9LHpJvSYMbJBDZ3oicFjWf8zdoXD~ZeXTf3yJUhfvyCnpwz~hzrgpCRoMAYQbdrqdB2VNvKh8cJehsN-XBtTslfx7HjhBbkJwll~Js7scLiPzPmN-rp8PqIRB~fjCQL0xgqocI0~LnVpYAwXhLjBUP~ketSartDVM2t4esVa8l8XBPNLkdLheHGb2XbyfxcSY692DHN8C6rVoPi7Ko7Eh3ZggbbU5AwES1n10Z50Q__"
                 alt=""
                 className="map"
               />
